c7f0db1e6244a8f15e1a18ae7ae34dd8542a0151
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-03-12 21:40:18 +0900

    Initial commit: 프로젝트 초기 세팅

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettierConfig from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  prettierConfig, // Prettier와 충돌하는 ESLint 규칙 비활성화
+];
