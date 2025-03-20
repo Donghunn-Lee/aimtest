16fd959509af403393e420b0d1edf9e6c710d149
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-03-20 21:34:25 +0900

    feat: 홈페이지 기본 구성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,14 @@ import GameView from '../components/Game/GameView';
 
 export default function HomePage() {
   return (
-    <main className="h-[600px] w-[800px] bg-gray-200">
-      <div className="mx-auto h-full w-full border border-gray-400">
+    <main className="mt-32 space-y-20">
+      <div className="space-y-12">
+        <h1 className="text-center text-5xl font-bold">
+          FPS Aim Test | 에임 테스트
+        </h1>
+        <p className="mx-auto w-[800px]"></p>
+      </div>
+      <div className="mx-auto h-[600px] w-[800px] border border-gray-400">
         <GameView />
       </div>
     </main>
