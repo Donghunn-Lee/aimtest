cfe4caf0dd33f635f10466ba3dd2c2c1ef0f0bc2
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-04-03 19:26:14 +0900

    feat: 크로스헤어 컴포넌트 추가

---
+++ src/components/Game/Crosshair.tsx
@@ -0,0 +1,7 @@
+export const Crosshair = () => {
+  return (
+    <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 select-none">
+      <div className="text-2xl text-red-500">+</div>
+    </div>
+  );
+};
