af93af16530822b6216d5e5ed0033abce4bdef09
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-05-01 11:20:02 +0900

    style: 패널 isVisible 속성 제거

--- src/components/common/PanelOverlay.tsx
+++ src/components/common/PanelOverlay.tsx
@@ -3,15 +3,26 @@ import { ReactNode } from 'react';
 interface PanelOverlayProps {
   children: ReactNode;
   className?: string;
+  animate?: boolean;
 }
 
-export const PanelOverlay = ({ children, className }: PanelOverlayProps) => {
+export const PanelOverlay = ({
+  children,
+  className,
+  animate = false,
+}: PanelOverlayProps) => {
   return (
-    <div
-      className={`fixed inset-0 flex items-center justify-center ${className}`}
-    >
-      <div className="rounded-lg bg-black bg-opacity-60 p-2 shadow-lg backdrop-blur-sm md:p-4 lg:p-6 xl:p-8">
-        {children}
+    <div className="fixed inset-0 flex items-center justify-center bg-opacity-50">
+      <div
+        className={`fixed inset-0 flex items-center justify-center ${
+          animate
+            ? `translate-y-0 opacity-100 transition-all duration-1000`
+            : ''
+        } ${className}`}
+      >
+        <div className="rounded-lg bg-black bg-opacity-60 p-2 shadow-lg backdrop-blur-sm md:p-4 lg:p-6 xl:p-8">
+          {children}
+        </div>
       </div>
     </div>
   );
