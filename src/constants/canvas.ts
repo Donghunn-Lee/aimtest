ae3dbd0df19e221fdba90b5f2b1d5b494c3ee18d
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-05-05 23:34:14 +0900

    feat: 타겟 캔바스 상수 추가

---
+++ src/constants/canvas.ts
@@ -0,0 +1,12 @@
+export const CANVAS_COLORS = {
+  BACKGROUND: '#000',
+  TARGET_OUTER: 'white',
+  TARGET_INNER: 'rgba(255, 0, 0, 1)',
+  TARGET_STROKE: '#333333',
+  TARGET_CENTER: 'white',
+} as const;
+
+export const CANVAS_STYLES = {
+  TARGET_CENTER_RADIUS: 1,
+  TARGET_FONT: '12px Arial',
+} as const;
