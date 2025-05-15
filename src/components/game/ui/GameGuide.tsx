eb615a05547a9c1f75abb99603f244042a0efd7e
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-05-15 21:12:53 +0900

    feat: 게임 가이드 항목 추가 및 수정

--- src/components/game/ui/GameGuide.tsx
+++ src/components/game/ui/GameGuide.tsx
@@ -19,20 +19,27 @@ const GameGuide = ({ animate = true }: GameGuideProps) => {
         Game Guide
       </h2>
       <ul className="list-inside list-disc space-y-1 text-xs md:space-y-1 lg:space-y-2 xl:space-y-2">
-        <li className={styles.listItem}>타겟을 클릭하여 점수를 획득하세요</li>
+        <li className={styles.listItem}>
+          <span className={styles.highlight}>점점 빠르게</span> 생성되는 타겟을
+          사격하여 점수를 획득하세요
+        </li>
         <li className={styles.listItem}>
           타겟 중앙부터 <span className={styles.highlight}>3점</span>,{' '}
           <span className={styles.highlight}>2점</span>,{' '}
           <span className={styles.highlight}>1점</span>의 점수를 얻습니다
         </li>
         <li className={styles.listItem}>
-          게임 시작 후 <span className={styles.highlight}>'['</span> 와{' '}
-          <span className={styles.highlight}>']'</span> 키로 마우스 민감도를
+          게임 시작 후 <span className={styles.highlight}>[</span> 와{' '}
+          <span className={styles.highlight}>]</span> 키로 마우스 민감도를
           조절할 수 있습니다
         </li>
         <li className={styles.listItem}>
           화면에 <span className={styles.highlight}>10개</span>의 타겟이
           남아있는 시점에 게임이 종료됩니다
+        </li>
+        <li className={styles.listItem}>
+          <span className={styles.highlight}>ESC 키</span>를 눌러 게임을 즉시
+          종료할 수 있습니다.
         </li>
       </ul>
     </div>
