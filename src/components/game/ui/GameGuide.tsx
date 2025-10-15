c157a46951e1df74a5f25157cb9340299af87ac0
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2025-10-15 14:38:12 +0900

    chore: 게임 가이드 수정
    - 변경된 게임 종료 기준 반영
    - 폰트 색상 변경

--- src/components/game/ui/GameGuide.tsx
+++ src/components/game/ui/GameGuide.tsx
@@ -7,7 +7,7 @@ interface GameGuideProps {
 const styles = {
   listItem:
     'text-[8px] leading-tight md:text-[9px] md:leading-snug lg:text-[10px] lg:leading-normal xl:text-xs xl:leading-relaxed',
-  highlight: 'text-orange-400 font-bold',
+  highlight: 'text-[#00ff00] font-bold',
 } as const;
 
 const GameGuide = ({ animate = true }: GameGuideProps) => {
@@ -15,14 +15,10 @@ const GameGuide = ({ animate = true }: GameGuideProps) => {
     <div
       className={`absolute left-4 top-4 max-w-[240px] space-y-2 rounded-lg bg-black bg-opacity-50 p-2 text-white backdrop-blur-sm transition-opacity duration-1000 md:left-4 md:top-4 md:max-w-[280px] md:p-2.5 lg:left-8 lg:top-8 lg:max-w-[320px] lg:p-3 xl:left-10 xl:top-10 xl:max-w-[360px] xl:p-3.5 ${animate ? 'opacity-100' : 'opacity-0'}`}
     >
-      <h2 className="text-[11px] font-bold md:text-xs lg:text-sm xl:text-base">
+      <h2 className="text-[11px] font-bold text-[#00ff00] md:text-xs lg:text-sm xl:text-base">
         Game Guide
       </h2>
       <ul className="list-inside list-disc space-y-1 text-xs md:space-y-1 lg:space-y-2 xl:space-y-2">
-        <li className={styles.listItem}>
-          <span className={styles.highlight}>점점 빠르게</span> 생성되는 타겟을
-          사격하여 점수를 획득하세요
-        </li>
         <li className={styles.listItem}>
           타겟 중앙부터 <span className={styles.highlight}>3점</span>,{' '}
           <span className={styles.highlight}>2점</span>,{' '}
@@ -34,8 +30,9 @@ const GameGuide = ({ animate = true }: GameGuideProps) => {
           조절할 수 있습니다
         </li>
         <li className={styles.listItem}>
-          화면에 <span className={styles.highlight}>10개</span>의 타겟이
-          남아있는 시점에 게임이 종료됩니다
+          타겟이 <span className={styles.highlight}>10개</span> 이상인 상태가{' '}
+          <span className={styles.highlight}>3초</span>간 지속될 경우 게임이
+          종료됩니다.
         </li>
         <li className={styles.listItem}>
           <span className={styles.highlight}>ESC</span>를 눌러 게임을 즉시
